b5816a9eadb34b6d2bee866ae91fd565755a9086
Hrant-p <hrant@live.ru>
2019-11-23 21:48:21 +0400

    4

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { lazy, Suspense } from "react";
-import "./App.scss";
+import audio from "./assets/audio/solo.mp3";
 import Spinner from "./components/Spinner";
+import "./App.scss";
 
 const GameBoard = lazy(() => import("./container/GameBoard"));
 
 function App() {
   return (
     <div className="App">
+      <audio src={audio} autoPlay>
+        <track kind="captions" />
+      </audio>
       <div className="App-body">
         <Suspense fallback={<Spinner />}>
           <GameBoard />
